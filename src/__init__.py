cda640934a616a2ec810016fc635c3ac178302d4
Ranjan Kumar <73190244+vatz-ranjan@users.noreply.github.com>
2022-08-26 07:31:19 +0530

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,4 +1,4 @@
 import mongoengine
 
-mongoengine.register_connection(host='mongodb+srv://vatz:vatz@cluster0.usw9cmv.mongodb.net/?retryWrites=true&w=majority', alias='core', name='Helper')
+mongoengine.register_connection(host='mongodb+srv://vatz:vatz@cluster0.usw9cmv.mongodb.net/?retryWrites=true&w=majority', alias='core', name='MissingChild')
 # mongoengine.register_connection(alias='core', name='Helper')
